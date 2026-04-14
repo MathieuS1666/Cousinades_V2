259b6dd17244c1721cfd92e87d0feff314474d36
MathieuS1666 <122774769+MathieuS1666@users.noreply.github.com>
2026-04-14 10:44:19 +0200

    Update script.js

--- script.js
+++ script.js
@@ -969,10 +969,7 @@ function afficherAllergies() {
 }
 function ouvrirAdmin() {
     const pass = prompt("Accès réservé. Veuillez saisir le mot de passe :");
-    
-    // Remplace '2026' par le mot de passe de ton choix
     if (pass === "1234") {
-        // Remplace 'TON_ID_SHEET' par l'identifiant réel de ton Google Sheet
         const urlSheet = "https://docs.google.com/spreadsheets/d/1F-Bx57myPupGgfFNAN79Pn8pQNON3aWg1pmF0jLFVNI/edit?usp=sharing";
         window.open(urlSheet, "_blank");
     } else if (pass !== null) { 
